63aea0d8ae6b066ad8207ed99c883e7a86b970d9
Kavia AI <production@kavia.ai>
2026-02-09 10:27:23 +0000

    Checkpoint 1 - cg62264940

---
+++ airport_facility_service/src/main/java/com/example/demo/airportfacilityserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.airportfacilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class airportfacilityserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(airportfacilityserviceApplication.class, args);
+	}
+
+}
